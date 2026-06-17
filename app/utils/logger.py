3eff41cf69e48b182d23e72d608011b1447c8b64
Vivin Anitha Thambidurai <vanithathambidurai1@sheffield.ac.uk>
2026-06-17 03:11:53 +0100

    Add structured logging for ingestion events closes #10

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,9 +1,32 @@
 import logging
+import sys
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+LOG_FORMAT = (
+    "%(asctime)s | %(levelname)s | %(name)s | "
+    "%(filename)s:%(lineno)d | %(message)s"
 )
 
-logger = logging.getLogger(__name__)
+
+def setup_logger(name: str = "api_ingestion_pipeline") -> logging.Logger:
+    """
+    Create a structured application logger for the ingestion pipeline.
+    """
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter(LOG_FORMAT))
+
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    return logger
+
+
+logger = setup_logger()
